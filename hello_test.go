fa83ef314c96237e9b9b72ededae3c7282b7fce0
Noa Lea <noa@noalea.work>
2022-04-19 15:38:21 -0400

    Finish `Hello, World` section

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,38 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Noa", "")
+		want := "Hello, Noa!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Noa", "Spanish")
+		want := "Hola, Noa!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Noa", "French")
+		want := "Bonjour, Noa!"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
